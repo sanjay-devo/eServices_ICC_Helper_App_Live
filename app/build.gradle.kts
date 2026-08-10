e22434d9fc5f352a1f301aad595fd1de94731810
Sanjay Devo <sanjaykumarvaishya.com@gmail.com>
2026-08-10 18:01:53 +0530

    1.1.4 update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.icc.eserviceshelper"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.1.3"
+        versionCode = 7
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/icc/eserviceshelper/utils/ContentProvider.kt
+++ app/src/main/java/com/icc/eserviceshelper/utils/ContentProvider.kt
@@ -235,9 +235,9 @@ object ContentProvider {
 
     private fun getAppInfoItems(): List<InfoItem> = listOf(
         InfoItem.AppInfo("App Name", "eServices: India Cyber Cafe", R.drawable.ic_info),
-        InfoItem.AppInfo("Current Version", "1.1.3", R.drawable.ic_new_releases),
-        InfoItem.AppInfo("Build Number", "6", R.drawable.ic_description),
-        InfoItem.AppInfo("Last Updated", "July 2026", R.drawable.ic_system_update),
+        InfoItem.AppInfo("Current Version", "1.1.4", R.drawable.ic_new_releases),
+        InfoItem.AppInfo("Build Number", "7", R.drawable.ic_description),
+        InfoItem.AppInfo("Last Updated", "Aug 2026", R.drawable.ic_system_update),
         InfoItem.AppInfo("Package Name", "com.icc.eserviceshelper", R.drawable.ic_public)
     )
 
